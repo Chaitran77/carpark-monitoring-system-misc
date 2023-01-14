3ccb3a3d370b4b197b5b6ccb6faf23cb6558d8de
Kiran Patel <kiran@inteleyes.com>
2023-01-14 17:25:15 +0000

    Update SQL-command-history.sql

--- SQL-command-history.sql
+++ SQL-command-history.sql
@@ -1,8 +1,12 @@
 -- CREATE DOMAIN numberplateType AS CHARACTER(7)
--- CONSTRAINT checkNumberplateFormat CHECK (VALUE IS NOT NULL AND VALUE ~ '?<Current>^[A-Z]{2}[0-9]{2}[A-Z]{3}$)|(?<Prefix>^[A-Z][0-9]{1,3}[A-Z]{3}$)|(?<Suffix>^[A-Z]{3}[0-9]{1,3}[A-Z]$)|(?<DatelessLongNumberPrefix>^[0-9]{1,4}[A-Z]{1,2}$)|(?<DatelessShortNumberPrefix>^[0-9]{1,3}[A-Z]{1,3}$)|(?<DatelessLongNumberSuffix>^[A-Z]{1,2}[0-9]{1,4}$)|(?<DatelessShortNumberSufix>^[A-Z]{1,3}[0-9]{1,3}$)|(?<DatelessNorthernIreland>^[A-Z]{1,3}[0-9]{1,4}$)|(?<DiplomaticPlate>^[0-9]{3}[DX]{1}[0-9]{3}$');
+-- CONSTRAINT checkNumberplateFormat CHECK (VALUE IS NOT NULL AND VALUE ~ '^((?:[A-HJ-PR-Y][A-HJ-PR-Y]\s?(?:0[2-9]|[1-9][0-9])\s?[A-HJ-PR-Z]{3})|(?:[A-Z](?:[IZ][A-Z]|[A-Z][IZ])\s?\d{1,4})|(?:[A-HJ-NP-TV-Y]\s?\d{1,3}\s?[A-Z]{3})|(?:[A-Z]{3}\s?\d{1,3}\s?[A-HJ-NPR-TV-Y])|(?:\d{1,3}\s?[A-HJ-Y]{1,3}|[A-HJ-Y]{1,3}\s?\d{1,3})|(?:\d{1,4}\s?(?:[IZ][A-Z]|[A-Z][IZ]))|(?:[A-Z]{1,2}\s?\d{1,4}))$');
 
-CREATE TABLE Vehicle (
-  VehicleID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  Numberplate numberplateType,
-  TenantID INTEGER
-)
+-- CREATE TABLE Vehicle (
+--   VehicleID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--   Numberplate numberplateType,
+--   TenantID INT
+-- )
+
+INSERT INTO Vehicle (Numberplate, TenantID) VALUES ('Y78 NOY', 0);
+
+-- DROP DOMAIN numberplateType;
