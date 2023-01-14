7e5691d3d3dd0c5e1e111fda78449970e1bf468a
Kiran Patel <kiran@inteleyes.com>
2023-01-14 17:24:57 +0000

    Update SQL-command-history.sql

--- SQL-command-history.sql
+++ SQL-command-history.sql
@@ -1,1 +1,8 @@
+-- CREATE DOMAIN numberplateType AS CHARACTER(7)
+-- CONSTRAINT checkNumberplateFormat CHECK (VALUE IS NOT NULL AND VALUE ~ '?<Current>^[A-Z]{2}[0-9]{2}[A-Z]{3}$)|(?<Prefix>^[A-Z][0-9]{1,3}[A-Z]{3}$)|(?<Suffix>^[A-Z]{3}[0-9]{1,3}[A-Z]$)|(?<DatelessLongNumberPrefix>^[0-9]{1,4}[A-Z]{1,2}$)|(?<DatelessShortNumberPrefix>^[0-9]{1,3}[A-Z]{1,3}$)|(?<DatelessLongNumberSuffix>^[A-Z]{1,2}[0-9]{1,4}$)|(?<DatelessShortNumberSufix>^[A-Z]{1,3}[0-9]{1,3}$)|(?<DatelessNorthernIreland>^[A-Z]{1,3}[0-9]{1,4}$)|(?<DiplomaticPlate>^[0-9]{3}[DX]{1}[0-9]{3}$');
 
+CREATE TABLE Vehicle (
+  VehicleID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  Numberplate numberplateType,
+  TenantID INTEGER
+)
