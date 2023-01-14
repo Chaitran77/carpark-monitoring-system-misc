9d2e61055c243148983fe69a8362bb9d5e51d0f4
Kiran Patel <kiran@inteleyes.com>
2023-01-14 17:26:07 +0000

    Update SQL-command-history.sql

--- SQL-command-history.sql
+++ SQL-command-history.sql
@@ -1,4 +1,4 @@
--- CREATE DOMAIN numberplateType AS CHARACTER(7)
+-- CREATE DOMAIN numberplateType AS CHARACTER(8)
 -- CONSTRAINT checkNumberplateFormat CHECK (VALUE IS NOT NULL AND VALUE ~ '^((?:[A-HJ-PR-Y][A-HJ-PR-Y]\s?(?:0[2-9]|[1-9][0-9])\s?[A-HJ-PR-Z]{3})|(?:[A-Z](?:[IZ][A-Z]|[A-Z][IZ])\s?\d{1,4})|(?:[A-HJ-NP-TV-Y]\s?\d{1,3}\s?[A-Z]{3})|(?:[A-Z]{3}\s?\d{1,3}\s?[A-HJ-NPR-TV-Y])|(?:\d{1,3}\s?[A-HJ-Y]{1,3}|[A-HJ-Y]{1,3}\s?\d{1,3})|(?:\d{1,4}\s?(?:[IZ][A-Z]|[A-Z][IZ]))|(?:[A-Z]{1,2}\s?\d{1,4}))$');
 
 -- CREATE TABLE Vehicle (
@@ -7,6 +7,6 @@
 --   TenantID INT
 -- )
 
-INSERT INTO Vehicle (Numberplate, TenantID) VALUES ('Y78 NOY', 0);
+INSERT INTO Vehicle (Numberplate, TenantID) VALUES ('TN99DRT', 0);
 
 -- DROP DOMAIN numberplateType;
