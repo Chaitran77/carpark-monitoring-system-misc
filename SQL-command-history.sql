9727ba51ff0487885777ed2b325efdf55b54394e
Kiran Patel <kiran@inteleyes.com>
2023-02-16 14:47:21 +0000

    Added more test screenshots and updated SQL-command-history.sql

--- SQL-command-history.sql
+++ SQL-command-history.sql
@@ -15,3 +15,28 @@ INSERT INTO "Carpark" (total_spaces, used_spaces) VALUES (50, 0);
 INSERT INTO "Camera" (ip_address, event_url, response_format, carpark_id) VALUES ('80.209.154.135', '/NotificationInfo/TollgateInfo', '', 1);
 
 SELECT * FROM "Camera", "Carpark";
+
+-- UPDATE "Carpark" SET used_spaces = used_spaces + -1;
+-- UPDATE "Log" SET exit_timestamp = '2020-05-27 20:00:13+00', exit_image_base64 = '' WHERE log_id = (SELECT MAX(log_id) FROM "Log" WHERE "Log".numberplate = '4R30SLK');
+
+
+-- INSERT INTO "Log" (camera_id, numberplate, entry_timestamp, entry_image_base64) VALUES (1, 'SZ66KYT', '2020-05-27 20:00:13+00', '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P//gf+ARAA7gAAAAAAAAAAAAAAAAEAA...nG1BwD2/Omw3tvKmyBZHdifkVcYHrmgD/2Q==');
+-- SELECT * FROM "Log";
+
+-- SELECT MAX("Log".log_id) FROM "Log" WHERE "Log".numberplate = '4R30SLK';
+
+-- INSERT INTO "Tenant" (forename, surname) VALUES ('John', 'Smith');
+-- INSERT INTO "Tenant" (forename, surname) VALUES ('William', 'Els');
+-- INSERT INTO "Tenant" (forename, surname) VALUES ('Dan', 'Pett');
+
+-- SELECT * FROM "Tenant";
+-- INSERT INTO "Vehicle" (numberplate, tenant_id) VALUES ('4R30SLK', 1);
+-- INSERT INTO "Vehicle" (numberplate, tenant_id) VALUES ('SZ66KYT', 1);
+-- INSERT INTO "Vehicle" (numberplate, tenant_id) VALUES ('S21NYG', 1);
+
+-- SELECT * FROM "Tenant", "Vehicle" WHERE "Vehicle".tenant_id = "Tenant".tenant_id;
+
+SELECT * FROM "Camera";
+
+UPDATE "Carpark" SET used_spaces = used_spaces + 1 FROM "Camera" WHERE "Carpark".carpark_id = "Camera".carpark_id AND "Camera".ip_address = '80.209.154.135';
+SELECT * FROM "Carpark";
